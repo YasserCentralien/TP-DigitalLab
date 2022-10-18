fa1bb7236975ac6032c5b0ed8305b06a814c12e1
Yasser AIT LAZIZ <99833348+YasserCentralien@users.noreply.github.com>
2022-10-18 18:08:15 +0200

    Add files via upload

---
+++ Projet blanc/dash_demo_heruko/app.py
@@ -0,0 +1,109 @@
+import numpy
+import pandas as pd
+import plotly.express as px
+from dash import Dash, html, dcc
+
+
+def to_categorical(column, bin_size=5, min_cut=15, max_cut=50):
+    if min_cut is None:
+        min_cut = int(round(column.min())) - 1
+    value_max = int(numpy.ceil(column.max()))
+    max_cut = min(max_cut, value_max)
+    intervals = [(x, x + bin_size) for x in range(min_cut, max_cut, bin_size)]
+    if max_cut != value_max:
+        intervals.append((max_cut, value_max))
+    return pd.cut(column, pd.IntervalIndex.from_tuples(intervals))
+
+
+url = 'https://www.famaf.unc.edu.ar/~nocampo043/sysarmy_survey_2020_processed.csv'
+df = pd.read_csv(url)
+
+df_fp = df[df["work_contract_type"].isin(["Full-Time", "Part-Time"])]
+
+salary_vs_contract_sc_fig = px.scatter(df_fp,
+                                       y="salary_monthly_GROSS",
+                                       x="salary_monthly_NET",
+                                       color="work_contract_type",
+                                       color_discrete_sequence=["lightseagreen", "indianred"])
+
+salary_vs_contract_sc_fig.update_layout(
+    plot_bgcolor="rgba(0, 0, 0, 0)",
+    paper_bgcolor="rgba(0, 0, 0, 0)",
+    font_color="white",
+)
+
+salary_vs_contract_box_fig = px.box(df_fp,
+                                    x="work_contract_type",
+                                    y="salary_monthly_NET",
+                                    points="all", color_discrete_sequence=["lightseagreen"])
+
+salary_vs_contract_box_fig.update_layout(
+    plot_bgcolor="rgba(0, 0, 0, 0)",
+    paper_bgcolor="rgba(0, 0, 0, 0)",
+    font_color="white",
+    yaxis_range=[0,1000000]
+)
+
+# salary_vs_contract_box_fig.update_layout(height=700, width=900)
+
+
+
+
+############################################################################################################################################
+
+external_stylesheets = [{"href": "./assets/styles.css", "rel": "stylesheet"}]
+app = Dash(__name__, external_stylesheets=external_stylesheets)
+server = app.server
+app.layout = html.Div(children=[
+    # Header
+    html.Div(
+        children=[
+            # Title
+            html.Div(children=[
+                html.H1(children="The impact of Covid 19 on the World Economy"),
+                html.
+                P(children=
+                  "Comparing the evolution of Covid 19 and that of the global economy",
+                  className="header__title")
+            ]),
+            html.Img(src="./assets/dash-new-logo.png",
+                     className="header__logo")
+        ],
+        className="header"),
+    # Content
+    html.Div(children=[
+        html.Div(children=[html.Img(src="./assets/Cumulative_cases.png",
+                     className=""),html.Img(src="./assets/GPD.png",
+                     className="")], className="right_plot column"),
+        html.Div(children=[html.P(children="Interpretation",className='H2'),
+                html.
+                P(children=
+                  " - As figure 1 shows, Gross Domestic Products per capita (GDP) acts as a metric for determining a country's economic output per each person living there.",
+                  className="paragraphe"),html.
+                P(children=
+                  " - As figure 1 shows, GDP fallen in Japan and Italy compared to Brazil after COVID-19. India and China had the highest GDP growth rates.",
+                  className="paragraphe"),
+                html.
+                P(children=
+                  " - Figure 2 shows that Inflation growth rates in India and Brazil were high during the pandemic.",
+                  className="paragraphe"),
+                html.
+                P(children=
+                  " - Populations in Germany and Japan declined significantly after 2019 based on figure 3.",
+                  className="paragraphe"),
+                html.
+                P(children=
+                  " Conclusion : Based on our research and datasets, the Covid-2019 did not have a significant impact on the main economy indicator(GDP).",
+                  className="paragraphe")], className="middle_plot column"),
+        html.Div(children=[
+            html.Img(src="./assets/inflation.png",
+                     className=""),
+            html.Img(src="./assets/population.png",
+                     className="")
+        ], className="right_plot column")
+    ],                      
+             className="content")
+], className="container")
+
+if __name__ == '__main__':
+    app.run_server(debug=True)
